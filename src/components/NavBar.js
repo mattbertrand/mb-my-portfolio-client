828adc23dda9951f3835a09bd971ad61f06d74a2
github username <github email address>
2021-04-08 16:32:47 -0700

    added about link to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,6 +7,7 @@ class NavBar extends Component {
             <div>
                 <ul>
                     <li><Link to="/">Home</Link></li>
+                    <li><Link to="/about">About Me</Link></li>
                 </ul>
             </div>
         )
