d83c72e6d7d182a801d3537a87a687440c1560fe
github username <github email address>
2021-04-08 16:17:56 -0700

    added an About Me component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class AboutMe extends Component {
+    render() {
+        return (
+            <div>
+                About Me
+            </div>
+        )
+    }
+}
+
+export default AboutMe
