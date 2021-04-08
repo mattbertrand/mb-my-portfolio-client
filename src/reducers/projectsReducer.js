fe024c44ce90a0b78ac0e16134cc5cc5e1861515
github username <github email address>
2021-04-08 16:27:54 -0700

    set the state to be equal to initialState

--- src/reducers/projectsReducer.js
+++ src/reducers/projectsReducer.js
@@ -3,7 +3,7 @@ const initialState = {
     loading: true
 }
 
-const projectsReducer = (state, action) => {
+const projectsReducer = (state=initialState, action) => {
     switch(action.type) {
         default:
             return state
