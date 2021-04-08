831492553e273adadce96de2a5e43ff6216f7dd8
github username <github email address>
2021-04-08 16:09:32 -0700

    created a projectsReducer

---
+++ src/reducers/projectsReducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+    projects: [],
+    loading: true
+}
+
+const projectsReducer = (state, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
