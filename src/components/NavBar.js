196cad7109b4649ed910f992c8aa0d827d111ee3
github username <github email address>
2021-04-08 16:30:06 -0700

    added first link to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class NavBar extends Component {
     render() {
         return (
             <div>
-                NavBar
+                <ul>
+                    <li><Link to="/">Home</Link></li>
+                </ul>
             </div>
         )
     }
