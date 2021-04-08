38bd89ff873ae3eb3e5e5bd3ce7decfc01522f4b
github username <github email address>
2021-04-08 16:26:50 -0700

    set up the error page for bad requests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import HomePage from './components/HomePage'
+import ErrorPage from './components/ErrorPage'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <NavBar />
         <Switch>
           <Route exact path="/" component={ HomePage } />
+          <Route component={ ErrorPage } />
         </Switch>
         <Footer />
       </Router>
