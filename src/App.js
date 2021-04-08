714bed1ee592cb0d95d42788d7627f6284fa54f9
github username <github email address>
2021-04-08 16:21:44 -0700

    imported Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 
 class App extends Component {
   render() {
     return (
+      <Router>
+        <NavBar />
       <div className="App">
         <h1>Hello World</h1>
       </div>
+      </Router>
     );
   }
   
