21219a88e6053781cfe5d56ff5084faee5e43062
github username <github email address>
2021-04-08 17:04:34 -0700

    created a form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
+    state = {
+        title: '',
+        category: '',
+        image_link: ''
+    }
+
     render() {
         return (
             <div>
-               Form 
+               <h3>Add a Project</h3>
+               <form>
+                   <div>
+                       <label htmlFor="title">Title</label>
+                       <input type="text" id="title" name="title" value={this.state.title} onChange={ this.handleChange }/>
+                   </div>
+                   <div>
+                       <label htmlFor="category">Category</label>
+                       <input type="text" id="category" name="category" value={this.state.category} onChange={ this.handleChange }/>
+                   </div>
+                   <div>
+                       <label htmlFor="image">Image</label>
+                       <input type="text" id="image" name="image" value={this.state.image} onChange={ this.handleChange }/>
+                   </div>
+                   <input type="submit" value="Add Project" />
+               </form>
             </div>
         )
     }
