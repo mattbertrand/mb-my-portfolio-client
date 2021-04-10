385a1cf97c557cbb4a0f64172f323d8994f28220
github username <github email address>
2021-04-09 19:18:29 -0700

    added a resume component

---
+++ src/components/Resume.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Resume extends Component {
+    render() {
+        return (
+            <div className="resume">
+                    <img alt="My Resume" src=""/>
+            </div>
+        )
+    }
+}
+
+export default Resume
