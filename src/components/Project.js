4b90698809165c5dbfcdcb4f821ff7dbf9bf9432
github username <github email address>
2021-04-10 13:52:06 -0700

    added  blog button and styled the project cards

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { deleteProject } from '../actions/index'
+import { Link } from 'react-router-dom'
 
 class Project extends Component {
     render() {
         const { title, category, image_link, id } = this.props
 
         return (
-            <div className="projects" >
-                <h3>{ title }</h3>
-                <p>{ category }</p>
-                <img alt="Project"src={ image_link }/>
-                <br />
-                <button className="button-color" onClick={() => this.props.deleteProject(id, this.props.history)}>Delete Project</button>
+            // <div className="projects" >
+            //     <h3>{ title }</h3>
+            //     <p>{ category }</p>
+            //     <img alt="Project"src={ image_link }/>
+            //     <br />
+            //     <button className="button-color" onClick={() => this.props.deleteProject(id, this.props.history)}>Delete Project</button>
+            // </div>
+            <div className="card">
+            <div className="card-body">
+                <div className="projects" >
+                    <h3>{ title }</h3>
+                    <p>{ category }</p>
+                    <img alt="Project"src={ image_link }/>
+                    <br />
+                    <button className="button-color" onClick={() => this.props.deleteProject(id, this.props.history)}>Delete Project</button>
+                    <br />
+                    <button className="button-color"><Link to="#" />Blog</button>
+                    
+                </div>
+
             </div>
+          </div>
+            
         )
     }
 }
