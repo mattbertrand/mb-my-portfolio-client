9a78975b2a9190556e0aec30f8ae9cc705ffbbdc
github username <github email address>
2021-04-10 14:00:05 -0700

    styled the error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
 class ErrorPage extends Component {
     render() {
         return (
-            <div>
-                Error Page
+            <div class="mainbox">
+                <div class="err">4</div>
+                <i class="far fa-question-circle fa-spin"></i>
+                <div class="err2">4</div>
+                <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="/">home</a> and try from there.</p></div>
             </div>
         )
     }
