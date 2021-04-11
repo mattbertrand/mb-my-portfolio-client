ce6f7c5d5636d642671585ca275c3082edd139c2
github username <github email address>
2021-04-11 12:46:20 -0700

    added a blogsReducer

---
+++ src/reducers/blogsReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    blogs: [],
+    loading: true
+}
+
+const blogsReducer = (state=initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
+
+export default blogsReducer
