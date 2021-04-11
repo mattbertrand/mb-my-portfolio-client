7aca46e50241055f74021157a6a4962090ac40b0
github username <github email address>
2021-04-11 13:35:40 -0700

    added a dispatch and action to set the blogs

--- src/actions/blogs/blog.js
+++ src/actions/blogs/blog.js
@@ -1,5 +1,8 @@
 export const getBlogs = () => {
     return dispatch => {
         dispatch({ type: "LOADING" })
+        fetch('http://localhost:3001/blogs')
+        .then(resp => resp.json())
+        .then(blogs => dispatch({ type: "SET_BLOGS" }))
     }
 }
--- src/reducers/blogsReducer.js
+++ src/reducers/blogsReducer.js
@@ -5,6 +5,17 @@ const initialState = {
 
 const blogsReducer = (state=initialState, action) => {
     switch(action.type) {
+        case "LOADING":
+            return {
+                ...state,
+                loading: true
+            }
+        case "SET_BLOGS":
+            return {
+                ...state,
+                loading: false,
+                blogs: action.blogs
+            }
         default:
             return state
     }
