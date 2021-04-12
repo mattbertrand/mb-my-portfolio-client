9fe636c6bdc852fc85dbf5bc342d7dc749e35b87
github username <github email address>
2021-04-11 22:07:34 -0700

    fixed error in errorPage className

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -3,11 +3,11 @@ import React, { Component } from 'react'
 class ErrorPage extends Component {
     render() {
         return (
-            <div class="mainbox">
-                <div class="err">4</div>
-                <i class="far fa-question-circle fa-spin"></i>
-                <div class="err2">4</div>
-                <div class="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="/">home</a> and try from there.</p></div>
+            <div className="mainbox">
+                <div className="err">4</div>
+                <i className="far fa-question-circle fa-spin"></i>
+                <div className="err2">4</div>
+                <div className="msg">Maybe this page moved? Got deleted? Is hiding out in quarantine? Never existed in the first place?<p>Let's go <a href="/">home</a> and try from there.</p></div>
             </div>
         )
     }
